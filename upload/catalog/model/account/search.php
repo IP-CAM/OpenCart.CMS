e5a9576c73acae4177c24df813d2a25d2ae8d5c8
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2019-10-11 20:47:49 +0200

    Красивый код

--- upload/catalog/model/account/search.php
+++ upload/catalog/model/account/search.php
@@ -8,8 +8,4 @@ class ModelAccountSearch extends Model {
 	public function addSearch($data) {
 		$this->db->query("INSERT INTO `" . DB_PREFIX . "customer_search` SET `store_id` = '" . (int)$this->config->get('config_store_id') . "', `language_id` = '" . (int)$this->config->get('config_language_id') . "', `customer_id` = '" . (int)$data['customer_id'] . "', `keyword` = '" . $this->db->escape($data['keyword']) . "', `category_id` = '" . (int)$data['category_id'] . "', `sub_category` = '" . (int)$data['sub_category'] . "', `description` = '" . (int)$data['description'] . "', `products` = '" . (int)$data['products'] . "', `ip` = '" . $this->db->escape($data['ip']) . "', `date_added` = NOW()");
 	}
-
-	public function addBlogSearch($data) {
-		$this->db->query("INSERT INTO `" . DB_PREFIX . "customer_blog_search` SET `store_id` = '" . (int)$this->config->get('config_store_id') . "', `language_id` = '" . (int)$this->config->get('config_language_id') . "', `customer_id` = '" . (int)$data['customer_id'] . "', `keyword` = '" . $this->db->escape($data['keyword']) . "', blog_category_id` = '" . (int)$data['category_id'] . "', `sub_category` = '" . (int)$data['sub_category'] . "', `description` = '" . (int)$data['description'] . "', `articles` = '" . (int)$data['articles'] . "', `ip` = '" . $this->db->escape($data['ip']) . "', `date_added` = NOW()");
-	}
 }
