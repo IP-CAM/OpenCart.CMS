f4953409c493f98456e71301b376e98c7cde2709
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2019-11-06 15:35:35 +0200

    Стикеры фикс, красивый код, -лишний код

--- upload/catalog/controller/product/mostviewed.php
+++ upload/catalog/controller/product/mostviewed.php
@@ -44,27 +44,11 @@ class ControllerProductMostviewed extends Controller {
 			$this->max = $limit;
 		}
 
-		if ($this->config->get('seomanager_meta_title_mostviewed')) {
-			$this->document->setTitle($this->config->get('seomanager_meta_title_mostviewed'));
-		} else {
-			$this->document->setTitle($this->language->get('heading_title'));
-		}
+		$this->document->setTitle($this->language->get('heading_title'));
 
-		if ($this->config->get('seomanager_html_h1_mostviewed')) {
-			$data['heading_title'] = $this->config->get('seomanager_html_h1_mostviewed');
-		} else {
-			$data['heading_title'] = $this->language->get('heading_title');
-		}
+		$data['heading_title'] = $this->language->get('heading_title');
 
-		if ($this->config->get('seomanager_meta_description_mostviewed')) {
-			$this->document->setDescription($this->config->get('seomanager_meta_description_mostviewed'));
-		}
-
-		if ($this->config->get('seomanager_meta_keyword_mostviewed')) {
-			$this->document->setKeywords($this->config->get('seomanager_meta_keyword_mostviewed'));
-		}
-
-		$data['description'] = html_entity_decode(($this->config->get('seomanager_description_mostviewed')), ENT_QUOTES, 'UTF-8');
+		$data['description'] = false;
 
 		$data['breadcrumbs'] = array();
 
