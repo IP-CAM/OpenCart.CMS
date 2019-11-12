d81156c6a04f308797a7bc32c8b3e0f0b7597a0b
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2019-11-12 16:09:21 +0200

    Кнопка быст. ред., полный путь, красивый код

--- upload/admin/controller/setting/setting.php
+++ upload/admin/controller/setting/setting.php
@@ -157,6 +157,8 @@ class ControllerSettingSetting extends Controller {
 		$data['entry_blog_full_path'] = $this->language->get('entry_blog_full_path');
 		$data['entry_manufacturers_full_path'] = $this->language->get('entry_manufacturers_full_path');
 		$data['entry_seo_url_postfix'] = $this->language->get('entry_seo_url_postfix');
+		$data['entry_valide_get_params_status'] = $this->language->get('entry_valide_get_params_status');
+		$data['entry_valide_get_params'] = $this->language->get('entry_valide_get_params');
 		$data['entry_compression'] = $this->language->get('entry_compression');
 		$data['entry_error_display'] = $this->language->get('entry_error_display');
 		$data['entry_error_log'] = $this->language->get('entry_error_log');
@@ -222,6 +224,8 @@ class ControllerSettingSetting extends Controller {
 		$data['help_blog_full_path'] = $this->language->get('help_blog_full_path');
 		$data['help_manufacturers_full_path'] = $this->language->get('help_manufacturers_full_path');
 		$data['help_seo_url_postfix'] = $this->language->get('help_seo_url_postfix');
+		$data['help_valide_get_params_status'] = $this->language->get('help_valide_get_params_status');
+		$data['help_valide_get_params'] = $this->language->get('help_valide_get_params');
 		$data['help_file_max_size'] = $this->language->get('help_file_max_size');
 		$data['help_file_ext_allowed'] = $this->language->get('help_file_ext_allowed');
 		$data['help_file_mime_allowed'] = $this->language->get('help_file_mime_allowed');
@@ -1127,24 +1131,24 @@ class ControllerSettingSetting extends Controller {
 			$data['config_seo_url_postfix'] = $this->config->get('config_seo_url_postfix');
 		}
 
-		if (isset($this->request->post['config_seo_url_valide_get_params_status'])) {
-			$data['config_seo_url_valide_get_params_status'] = $this->request->post['config_seo_url_valide_get_params_status'];
+		if (isset($this->request->post['config_valide_get_params_status'])) {
+			$data['config_valide_get_params_status'] = $this->request->post['config_valide_get_params_status'];
 		// удалить в 2.3.0.2.7
-		} elseif ($this->config->get('config_seo_url_valide_get_params_status') == null) {
-			$data['config_seo_url_valide_get_params_status'] = 1;
+		} elseif ($this->config->get('config_valide_get_params_status') == null) {
+			$data['config_valide_get_params_status'] = 1;
 		// удалить в 2.3.0.2.7
 		} else {
-			$data['config_seo_url_valide_get_params_status'] = $this->config->get('config_seo_url_valide_get_params_status');
+			$data['config_valide_get_params_status'] = $this->config->get('config_valide_get_params_status');
 		}
 
-		if (isset($this->request->post['config_seo_url_valide_get_params'])) {
-			$data['config_seo_url_valide_get_params'] = $this->request->post['config_seo_url_valide_get_params'];
+		if (isset($this->request->post['config_valide_get_params'])) {
+			$data['config_valide_get_params'] = $this->request->post['config_valide_get_params'];
 		// удалить в 2.3.0.2.7
-		} elseif ($this->config->get('config_seo_url_valide_get_params') == null) {
-			$data['config_seo_url_valide_get_params'] = 'tracking\r\nutm_source\r\nutm_campaign\r\nutm_medium\r\ntype\r\nsource\r\nblock\r\nposition\r\nkeyword\r\nyclid\r\ngclid';
+		} elseif ($this->config->get('config_valide_get_params') == null) {
+			$data['config_valide_get_params'] = 'tracking\r\nutm_source\r\nutm_campaign\r\nutm_medium\r\ntype\r\nsource\r\nblock\r\nposition\r\nkeyword\r\nyclid\r\ngclid';
 		// удалить в 2.3.0.2.7
 		} else {
-			$data['config_seo_url_valide_get_params'] = $this->config->get('config_seo_url_valide_get_params');
+			$data['config_valide_get_params'] = $this->config->get('config_valide_get_params');
 		}
 
 		if (isset($this->request->post['config_file_max_size'])) {
